dccd14d26b0e064a63fe92b4a44a112f01941e55
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-03-15 20:07:06 -0300

    Aula 'Inserindo com UUID' (C2-M1-SM2)

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use(categoriesRoutes);
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("🔥️ Server is running on port 3333 🔥️"));

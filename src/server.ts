0450e50cf03995c282556599522564d5e2b92d07
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-03-15 19:54:16 -0300

    Aula 'Criando categoria' (C2-M1-SM2)

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
 app.use(express.json());
+
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("🔥️ Server is running on port 3333 🔥️"));

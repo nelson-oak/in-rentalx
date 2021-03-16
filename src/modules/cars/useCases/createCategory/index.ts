34b9cc34db81e8a602ce8e2461688c7e687350f7
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-03-16 20:37:05 -0300

    Aula 'Refatorando a listagem de categoria' (C2-M1-SM4)

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -3,9 +3,7 @@ import { CreateCategoryController } from "./CreateCategoryController";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 const categoriesRepository = new CategoriesRepository();
-
 const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
-
 const createCategoryController = new CreateCategoryController(
   createCategoryUseCase
 );

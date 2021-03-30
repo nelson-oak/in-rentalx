31b90153c2a3512affdd713d942fba05ef2eb764
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-03-30 18:45:11 -0300

    Aula 'Continuando o caso de uso de carros' (C4-M1-SM3)

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -12,7 +12,7 @@ describe("Create Car", () => {
   });
 
   it("should be able to create a new car", async () => {
-    await createCarUseCase.execute({
+    const car = await createCarUseCase.execute({
       name: "Car's Name",
       description: "Car's Description",
       daily_rate: 1234,
@@ -21,5 +21,45 @@ describe("Create Car", () => {
       brand: "Car's Brand",
       category_id: "Car's Category ID",
     });
+
+    expect(car).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new car with an exists license plate", () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: "Car's Name",
+        description: "Car's Description",
+        daily_rate: 1234,
+        license_plate: "Car's License Plate",
+        fine_amount: 5678,
+        brand: "Car's Brand",
+        category_id: "Car's Category ID",
+      });
+
+      await createCarUseCase.execute({
+        name: "Car's Name 2",
+        description: "Car's Description 2",
+        daily_rate: 1234,
+        license_plate: "Car's License Plate 2",
+        fine_amount: 5678,
+        brand: "Car's Brand",
+        category_id: "Car's Category ID 2",
+      });
+    });
+  });
+
+  it("should be able to create a new car with available true by default", async () => {
+    const car = await createCarUseCase.execute({
+      name: "Car's Name",
+      description: "Car's Description",
+      daily_rate: 1234,
+      license_plate: "Car's License Plate",
+      fine_amount: 5678,
+      brand: "Car's Brand",
+      category_id: "Car's Category ID",
+    });
+
+    expect(car.available).toBe(true);
   });
 });

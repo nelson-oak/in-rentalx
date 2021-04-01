9ba4cae6d2a4160237ae45b66c0ee1fe8d6c693c
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-04-01 20:00:22 -0300

    Aula 'Continuação do cadastro de aluguel' (C4-M1-SM4)

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { AppError } from "@shared/errors/AppError";
 
@@ -19,7 +20,7 @@ class CreateRentalUseCase {
     user_id,
     car_id,
     expected_return_date,
-  }: IRequest): Promise<void> {
+  }: IRequest): Promise<Rental> {
     const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
       car_id
     );
@@ -35,6 +36,14 @@ class CreateRentalUseCase {
     if (rentalOpenToUser) {
       throw new AppError("There is a rental in progress for user");
     }
+
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return rental;
   }
 }
 

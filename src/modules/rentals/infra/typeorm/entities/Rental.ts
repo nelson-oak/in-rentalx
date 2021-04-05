910d796ac6184c41b8996b5a9ab176bd8b0e5582
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-04-05 20:03:56 -0300

    Aula 'Refatorando a listagem de aluguel do usuário' (C5-M1-SM1)

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -3,18 +3,22 @@ import {
   CreateDateColumn,
   Entity,
   JoinColumn,
+  ManyToOne,
   PrimaryColumn,
   UpdateDateColumn,
 } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
-import { User } from "@modules/accounts/infra/typeorm/entities/User";
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 
 @Entity("rentals")
 class Rental {
   @PrimaryColumn()
   id: string;
+
+  @ManyToOne(() => Car)
+  @JoinColumn({ name: "car_id" })
+  car: Car;
 
   @Column()
   car_id: string;

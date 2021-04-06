e075462684bd28494af0f3b0eeb01fadf505d9c5
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-04-06 19:36:27 -0300

    Aula 'Criando caso de uso do refresh token' (C5-M1-SM2)

---
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -0,0 +1,63 @@
+import { verify, sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import auth from "@config/auth";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { AppError } from "@shared/errors/AppError";
+
+interface IPayload {
+  email: string;
+  sub: string;
+}
+
+@injectable()
+class RefreshTokenUseCase {
+  constructor(
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: IUsersTokensRepository,
+
+    @inject("DayJsDateProvider")
+    private dateProvider: IDateProvider
+  ) {}
+
+  async execute(token: string): Promise<string> {
+    const {
+      secret_refresh_token,
+      expires_in_refresh_token,
+      expires_refresh_token_days,
+    } = auth;
+
+    const { email, sub } = verify(token, secret_refresh_token) as IPayload;
+
+    const user_id = sub;
+
+    const userToken = await this.usersTokensRepository.findByUserIdAndRefreshToken(
+      user_id,
+      token
+    );
+
+    if (!userToken) {
+      throw new AppError("Refresh Token does not exists!");
+    }
+
+    await this.usersTokensRepository.deleteById(userToken.id);
+
+    const refresh_token = sign({ email }, secret_refresh_token, {
+      subject: sub,
+      expiresIn: expires_in_refresh_token,
+    });
+
+    const expires_date = this.dateProvider.addDays(expires_refresh_token_days);
+
+    await this.usersTokensRepository.create({
+      refresh_token,
+      expires_date,
+      user_id: sub,
+    });
+
+    return refresh_token;
+  }
+}
+
+export { RefreshTokenUseCase };
